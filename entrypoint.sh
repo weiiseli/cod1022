fee97d59cf5565cc9497adcb38492b17cedbff74
weiiseli <132556039+weiiseli@users.noreply.github.com>
2023-10-23 04:53:08 -0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,310 @@
+﻿#!/usr/bin/env bash
+
+# 设置各变量
+UUID='34477103-9790-4091-b320-25bd02de3d4c'
+VMESS_WSPATH='/vmess'
+VLESS_WSPATH='/vless'
+TROJAN_WSPATH='/trojan'
+SS_WSPATH='/shadowsocks'
+NEZHA_SERVER=''
+NEZHA_PORT=''
+NEZHA_KEY=''
+NEZHA_TLS='1'
+
+# 检查 wget 命令
+check_wget() {
+  ISWGET=0 && [ "$(type -p wget)" ] && ISWGET=1
+}
+
+generate_config() {
+  cat > config.json << EOF
+{
+    "log": {
+        "access": "/dev/null",
+        "error": "/dev/null",
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "port": 8080,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}",
+                        "flow": "xtls-rprx-vision"
+                    }
+                ],
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": 3001
+                    },
+                    {
+                        "path": "${VLESS_WSPATH}",
+                        "dest": 3002
+                    },
+                    {
+                        "path": "${VMESS_WSPATH}",
+                        "dest": 3003
+                    },
+                    {
+                        "path": "${TROJAN_WSPATH}",
+                        "dest": 3004
+                    },
+                    {
+                        "path": "${SS_WSPATH}",
+                        "dest": 3005
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp"
+            }
+        },
+        {
+            "port": 3001,
+            "listen": "127.0.0.1",
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none"
+            }
+        },
+        {
+            "port": 3002,
+            "listen": "127.0.0.1",
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": "${VLESS_WSPATH}"
+                }
+            },
+            "sniffing": {
+                "enabled": false,
+                "destOverride": [
+                    "http",
+                    "tls"
+                ],
+                "metadataOnly": false
+            }
+        },
+        {
+            "port": 3003,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}",
+                        "alterId": 0
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "${VMESS_WSPATH}"
+                }
+            },
+            "sniffing": {
+                "enabled": false,
+                "destOverride": [
+                    "http",
+                    "tls"
+                ],
+                "metadataOnly": false
+            }
+        },
+        {
+            "port": 3004,
+            "listen": "127.0.0.1",
+            "protocol": "trojan",
+            "settings": {
+                "clients": [
+                    {
+                        "password": "${UUID}"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": "${TROJAN_WSPATH}"
+                }
+            },
+            "sniffing": {
+                "enabled": false,
+                "destOverride": [
+                    "http",
+                    "tls"
+                ],
+                "metadataOnly": false
+            }
+        },
+        {
+            "port": 3005,
+            "listen": "127.0.0.1",
+            "protocol": "shadowsocks",
+            "settings": {
+                "clients": [
+                    {
+                        "method": "chacha20-ietf-poly1305",
+                        "password": "${UUID}"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "${SS_WSPATH}"
+                }
+            },
+            "sniffing": {
+                "enabled": false,
+                "destOverride": [
+                    "http",
+                    "tls"
+                ],
+                "metadataOnly": false
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {}
+        },
+        {
+            "tag": "WARP",
+            "protocol": "wireguard",
+            "settings": {
+                "secretKey": "GAl2z55U2UzNU5FG+LW3kowK+BA/WGMi1dWYwx20pWk=",
+                "address": [
+                    "172.16.0.2/32",
+                    "2606:4700:110:8f0a:fcdb:db2f:3b3:4d49/128"
+                ],
+                "peers": [
+                    {
+                        "publicKey": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+                        "endpoint": "engage.cloudflareclient.com:2408"
+                    }
+                ]
+            }
+        }
+    ],
+    "routing": {
+        "domainStrategy": "AsIs",
+        "rules": [
+            {
+                "type": "field",
+                "domain": [
+                    "domain:openai.com",
+                    "domain:ai.com"
+                ],
+                "outboundTag": "WARP"
+            }
+        ]
+    },
+    "dns": {
+        "server": [
+            "8.8.8.8",
+            "8.8.4.4"
+        ]
+    }
+}
+EOF
+}
+
+generate_web() {
+  cat > web.sh << EOF
+#!/usr/bin/env bash
+
+check_file() {
+  [ ! -e web.js ] && wget -O web.js https://gitlab.com/Misaka-blog/xray-for-nodejs/-/raw/main/web.js
+}
+
+run() {
+  chmod +x web.js && ./web.js -c ./config.json >/dev/null 2>&1 &
+}
+
+check_file
+run
+EOF
+}
+
+generate_nezha() {
+  cat > nezha.sh << EOF
+#!/usr/bin/env bash
+
+# 哪吒的4个参数
+NEZHA_SERVER="$NEZHA_SERVER"
+NEZHA_PORT="$NEZHA_PORT"
+NEZHA_KEY="$NEZHA_KEY"
+NEZHA_TLS="$NEZHA_TLS"
+
+# 检测是否已运行
+check_run() {
+  pidof -q nezha-agent && echo "哪吒客户端正在运行中!" && exit
+}
+
+# 三个变量不全则不安装哪吒客户端
+check_variable() {
+  [[ -z "\${NEZHA_SERVER}" || -z "\${NEZHA_PORT}" || -z "\${NEZHA_KEY}" ]] && exit
+}
+
+# 下载最新版本 Nezha Agent
+download_agent() {
+  if [ ! -e nezha-agent ]; then
+    if [ "$ISWGET" = 1 ]; then
+      URL=\$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
+      wget -t 2 -T 10 -N \${URL}
+      unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
+    else
+      echo " wget is not found. "
+    fi
+  fi
+  chmod +x nezha-agent
+}
+
+# 运行客户端
+run() {
+  TLS=\${NEZHA_TLS:+'--tls'}
+  [[ ! \$PROCESS =~ nezha-agent && -e nezha-agent ]] && ./nezha-agent -s \${NEZHA_SERVER}:\${NEZHA_PORT} -p \${NEZHA_KEY} \${TLS} 2>&1 &
+}
+
+check_run
+check_variable
+download_agent
+run
+EOF
+}
+
+check_wget
+generate_config
+generate_web
+generate_nezha
+
+[ -e web.sh ] && bash web.sh
+[ -e nezha.sh ] && bash nezha.sh
---
+++ index.js
@@ -0,0 +1,137 @@
+﻿const server = "127.0.0.1";
+const port = "3000";
+const express = require("express");
+const app = express();
+var exec = require("child_process").exec;
+const os = require("os");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+var request = require("request");
+var fs = require("fs");
+var path = require("path");
+
+//首页显示内容
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+//获取系统监听端口
+app.get("/listen", function (req, res) {
+    let cmdStr = "ss -nltp";
+    exec(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+      }
+      else {
+        res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
+      }
+    });
+  });
+
+//获取系统进程表
+app.get("/status", function (req, res) {
+  let cmdStr = "ps -ef";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    }
+    else {
+      res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
+    }
+  });
+});
+
+//启动web
+app.get("/web", function (req, res) {
+  let cmdStr = "bash web.sh >/dev/null 2>&1 &";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("Web 执行错误：" + err);
+    }
+    else {
+      res.send("web start ok");
+    }
+  });
+});
+
+//启动哪吒
+app.get("/nezha", function (req, res) {
+  let cmdStr = "bash nezha.sh >/dev/null 2>&1 &";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("哪吒部署错误：" + err);
+    }
+    else {
+      res.send("nezha start ok");
+    }
+  });
+});
+
+//启动e
+app.get("/e", function (req, res) {
+  let cmdStr = "bash entrypoint.sh >/dev/null 2>&1 &";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("Web 执行错误：" + err);
+    }
+    else {
+      res.send("entrypoint start ok");
+    }
+  });
+});
+
+//获取系统版本、内存信息
+app.get("/info", function (req, res) {
+  let cmdStr = "cat /etc/*release | grep -E ^NAME";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("命令行执行错误：" + err);
+    }
+    else {
+      res.send(
+        "命令行执行结果：\n" +
+          "Linux System:" +
+          stdout +
+          "\nRAM:" +
+          os.totalmem() / 1000 / 1000 +
+          "MB"
+      );
+    }
+  });
+});
+
+//文件系统只读测试
+app.get("/test", function (req, res) {
+  fs.writeFile("./test.txt", "这里是新创建的文件内容!", function (err) {
+    if (err) {
+      res.send("创建文件失败，文件系统权限为只读：" + err);
+    }
+    else {
+      res.send("创建文件成功，文件系统权限为非只读：");
+    }
+  });
+});
+
+app.use(
+  "/",
+  createProxyMiddleware({
+    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
+    onProxyReq: function onProxyReq(proxyReq, req, res) {},
+    pathRewrite: {
+      // 请求中去除/
+      "^/": "/"
+    },
+    target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
+    ws: true // 是否代理websockets
+  })
+);
+
+//启动核心脚本运行web和哪吒
+exec('bash entrypoint.sh', function (err, stdout, stderr) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
